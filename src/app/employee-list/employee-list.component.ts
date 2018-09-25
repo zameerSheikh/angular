a7a1e98c5df22757caf3aa3773a50765c4528487
zameer <vinutawadi07@gmail.com>
2018-09-25 12:54:25 +0530

    integrated output and eventemmiter

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -8,16 +8,22 @@ import { Component } from "@angular/core";
 export class EmployeeListComponent {
   employees: any[];
 
+  selectedEmployeeCountRadioButton: string = 'all';
+
   constructor(){
   this.employees= [
     {code: 'emp01', name: 'Alex',  gender: 'male',    dob:'2-04-1993', salary: 5500},
     {code: 'emp02', name: 'Jones', gender: 'female',  dob:'8-07-1995', salary: 5700},
     {code: 'emp03', name: 'Mark',  gender: 'male',    dob:'6-09-1990', salary: 5800},
-    {code: 'emp04', name: 'Rose',  gender: 'female',  dob:'11-15-1994', salary: 6000}
+    {code: 'emp04', name: 'Rose',  gender: 'female',  dob:'11-15-1994', salary: 6000},
     {code: 'emp05', name: 'Addy',  gender: 'male',    dob:'10-19-1995', salary: 6500},
     {code: 'emp06', name: 'Tom',  gender: 'male',    dob:'5-21-1989', salary: 6500}
   ];
 }
+
+  onEmployeeCountRadioButtonChanged(selectedRadioButtonValue: string): void {
+    this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+  }
 
   getTotalEmpCount(): number {
     return this.employees.length;
--- src/app/employee/employeeCount.component.ts
+++ src/app/employee/employeeCount.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'emp-count',
     templateUrl: 'employeeCountComponent.html'
 })
 export class EmployeeCountComponent {
+
+    selectedRadioButtonValue: string = 'all';
+
+    @Output()
+    countRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
     @Input()
     all: number;
 
@@ -13,4 +19,8 @@ export class EmployeeCountComponent {
 
     @Input()
     female: number;
+
+    onRadioButtonSelectionChange() {
+        this.countRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
+    }
 }
